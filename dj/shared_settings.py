a8fa7dece9ea1e553b7b6d39820dbe99819e273b
Claudio Santini <hireclaudio@gmail.com>
2019-02-17 19:11:27 +0100

    fix

--- dj/shared_settings.py
+++ dj/shared_settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_rq",
+    # "django_rq",
     'crispy_forms',
     'rss',
 ]
@@ -148,20 +148,20 @@ CACHES = {
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-# CELERY STUFF
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Europe/London'
-
-RQ_QUEUES = {
-    'default': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'DB': 0,
-        # 'PASSWORD': 'some-password',
-        'DEFAULT_TIMEOUT': 360,
-    },
-}
+# # CELERY STUFF
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Europe/London'
+#
+# RQ_QUEUES = {
+#     'default': {
+#         'HOST': 'localhost',
+#         'PORT': 6379,
+#         'DB': 0,
+#         # 'PASSWORD': 'some-password',
+#         'DEFAULT_TIMEOUT': 360,
+#     },
+# }
